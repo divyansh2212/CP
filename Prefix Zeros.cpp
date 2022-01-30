08bf3b1457df4f5aaa5fc5717483c92dc59c378b
Divyansh Mittal <mittaldivyansh2212@gmail.com>
2022-01-30 21:34:22 +0530

    30/01/22

--- Prefix Zeros.cpp
+++ Prefix Zeros.cpp
@@ -3,42 +3,42 @@ using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
+    int tt;
+    cin >> tt;
+    while (tt--)
     {
         int n, k;
         cin >> n >> k;
         string s;
         cin >> s;
-        int cnt = 0;
-        for (int i = 0; i < s.length(); i++)
+        int low = 0;
+        int high = n - 1;
+        while (high - low > 1)
         {
-            if (s[i] == '0')
+            int mid = (high + low) >> 1;
+            int cnt = 0;
+            for (int i = mid; i >= 0; i--)
             {
-                cnt++;
-                continue;
+                int now = (cnt + s[i] - '0') % 10;
+                if (now != 0)
+                    cnt += 10 - now;
             }
-            int val = 10 - s[i] + '0';
-            if (val <= k)
-            {
-                cnt++;
-                k = k - val;
-            }
-            else
-            {
-                for (int j = i + 1; j < s.length(); j++)
-                {
-                    if (s[j] >= s[i])
-                        cnt++;
-                    else
-                        break;
-                }
-                break;
-            }
-        }
-        cout << cnt << endl;
-    }
 
+            if (cnt <= k)
+                low = mid;
+
+            else
+                high = mid - 1;
+        }
+        if (high == 0 && low == 0)
+        {
+            if (s[0] == '0')
+                cout << 1 << endl;
+            else
+                cout << 0 << endl;
+        }
+        else
+            cout << high + 1 << '\n';
+    }
     return 0;
 }
